6be0081bbb45a98cf7046b603d3d7af59f9caf99
Rob Brennan <rob@therobbrennan.com>
2024-03-04 01:34:57 -0800

    Implement the parallax effect for hero icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,23 @@
-import { curve, heroBackground, robot } from '../assets'
-import Button from './Button'
-import Section from './Section'
+import { curve, heroBackground, robot } from "../assets"
+import Button from "./Button"
+import Section from "./Section"
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero"
+import { heroIcons } from "../constants"
+import { ScrollParallax } from "react-just-parallax"
+import { useRef } from "react"
 
 const Hero = () => {
-  return (
-    <Section className={`pt-[12rem] -mt-[5.25rem]`}
-      crosses
-      crossesOffset={`lg:translate-y-[5.25rem]`}
-      customPaddings
-      id='hero'>
+  const parallaxRef = useRef(null)
 
-      <div className="container relative">
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25rem]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
@@ -46,9 +53,21 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+
               </div>
             </div>
 
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -60,10 +79,13 @@ const Hero = () => {
             />
           </div>
 
+          <BackgroundCircles />
         </div>
 
+        {/* <CompanyLogos className="hidden relative z-10 mt-20 lg:block" /> */}
       </div>
 
+      <BottomLine />
     </Section>
   )
 }
