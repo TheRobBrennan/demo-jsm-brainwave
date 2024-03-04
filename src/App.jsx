756c608514a84c4b6a6ef4537eb04a47c6f6ee50
Rob Brennan <rob@therobbrennan.com>
2024-03-04 00:46:57 -0800

    Add placeholder Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import Header from "./components/Header"
+import Hero from './components/Hero'
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       {/* We have defined padding top because the header is fixed and we don't want it to overlap with the content */}
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
