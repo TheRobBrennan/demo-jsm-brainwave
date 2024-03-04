fd610c0111e3d5bf3866fc38a986efe863c6e67d
Rob Brennan <rob@therobbrennan.com>
2024-03-04 03:58:35 -0800

    Remove superfluous comment

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Services from "./components/Services"
 const App = () => {
   return (
     <>
-      {/* We have defined padding top because the header is fixed and we don't want it to overlap with the content */}
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
