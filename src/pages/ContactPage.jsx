9af4a418fcf83c866734e5e8a2f2c1f5d8a89083
Rob Brennan <rob@therobbrennan.com>
2024-03-05 01:41:44 -0800

    Update placeholder content for Contact and Demo pages

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,3 +1,18 @@
-const ContactPage = () => <div>Contact Page Content</div>
+import Section from "../components/Section"
+
+const ContactPage = () => {
+  return (<Section
+    className="pt-[6rem] -mt-[6rem]"
+    crosses
+    crossesOffset="lg:translate-y-[5.25rem]"
+    customPaddings
+    id="hero"
+  >
+    <div className="container relative z-1 max-w-[62rem] mx-auto text-center align-top">
+      Contact Page Content
+    </div>
+  </Section>
+  )
+}
 
 export default ContactPage
--- src/pages/DemoPage.jsx
+++ src/pages/DemoPage.jsx
@@ -1,3 +1,18 @@
-const DemoPage = () => <div>Demo Page Content</div>
+import Section from "../components/Section"
+
+const DemoPage = () => {
+  return (<Section
+    className="pt-[6rem] -mt-[6rem]"
+    crosses
+    crossesOffset="lg:translate-y-[5.25rem]"
+    customPaddings
+    id="hero"
+  >
+    <div className="container relative z-1 max-w-[62rem] mx-auto text-center align-top">
+      Demo Page Content
+    </div>
+  </Section>
+  )
+}
 
 export default DemoPage
